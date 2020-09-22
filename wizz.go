cc79826d86306cc0102e0ea837556bc3664b59e4
Ferdina <ferdina.kusumah@gmail.com>
2020-09-22 17:51:20 +0700

    Change module name

--- wizz.go
+++ wizz.go
@@ -1,99 +1,95 @@
 package wizz
 
 import (
-	"encoding/json"
-	"flag"
-	"fmt"
 	"github.com/FerdinaKusumah/wizz/connection"
 	"github.com/FerdinaKusumah/wizz/models"
 	"github.com/FerdinaKusumah/wizz/utils"
 	"log"
-	"os"
 	"time"
 )
 
-var (
-	bulbIp, method string
-	value, r, g, b float64
-	sceneId        int64
-)
-
-func flags() {
-	flag.StringVar(&bulbIp, "bulbIp", "192.168.1.9", "Port buob in local network")
-	flag.Float64Var(&value, "value", 20.0, "Value to adjust in bulb")
-	flag.Float64Var(&r, "r", 20.0, "Value to adjust in RGB Mode")
-	flag.Float64Var(&g, "g", 20.0, "Value to adjust in RGB Mode")
-	flag.Float64Var(&b, "b", 1.0, "Value to adjust in RGB Mode")
-	flag.Int64Var(&sceneId, "sceneId", 20.0, "Set bulb in specific scene mode")
-	flag.StringVar(&method, "method", "GetState", "Available method is GetState, GetConfig, TurnOnLight, TurnOffLight, SetColorTemp, SetBrightness, SetColorRGB, SetColorScene, SetColorWarmWhite, SetColorColdWhite")
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-}
-
-func init() {
-	flags()
-}
-
-func main() {
-	flag.Parse()
-
-	var (
-		response = new(models.ResponsePayload)
-		result   []byte
-		err      error
-	)
-	switch method {
-	case "GetState":
-		if response, err = GetState(bulbIp); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "GetConfig":
-		if response, err = GetConfig(bulbIp); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "TurnOnLight":
-		if response, err = TurnOnLight(bulbIp); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "TurnOffLight":
-		if response, err = TurnOffLight(bulbIp); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "SetColorTemp":
-		if response, err = SetColorTemp(bulbIp, value); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "SetBrightness":
-		if response, err = SetBrightness(bulbIp, value); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "SetColorRGB":
-		if response, err = SetColorRGB(bulbIp, r, g, b); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "SetColorScene":
-		if response, err = SetColorScene(bulbIp, sceneId); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "SetColorWarmWhite":
-		if response, err = SetColorWarmWhite(bulbIp, value); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	case "SetColorColdWhite":
-		if response, err = SetColorColdWhite(bulbIp, value); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	default:
-		if response, err = GetState(bulbIp); err != nil {
-			log.Fatalf(`Unable to read response: %s`, err)
-		}
-	}
-	if result, err = json.Marshal(response); err != nil {
-		log.Fatalf(`Unable to convert to json string: %s`, err)
-	}
-	fmt.Println(string(result))
-	os.Exit(0)
-}
+//var (
+//	bulbIp, method string
+//	value, r, g, b float64
+//	sceneId        int64
+//)
+//
+//func flags() {
+//	flag.StringVar(&bulbIp, "bulbIp", "192.168.1.9", "Port buob in local network")
+//	flag.Float64Var(&value, "value", 20.0, "Value to adjust in bulb")
+//	flag.Float64Var(&r, "r", 20.0, "Value to adjust in RGB Mode")
+//	flag.Float64Var(&g, "g", 20.0, "Value to adjust in RGB Mode")
+//	flag.Float64Var(&b, "b", 1.0, "Value to adjust in RGB Mode")
+//	flag.Int64Var(&sceneId, "sceneId", 20.0, "Set bulb in specific scene mode")
+//	flag.StringVar(&method, "method", "GetState", "Available method is GetState, GetConfig, TurnOnLight, TurnOffLight, SetColorTemp, SetBrightness, SetColorRGB, SetColorScene, SetColorWarmWhite, SetColorColdWhite")
+//	log.SetOutput(os.Stdout)
+//	log.SetFlags(log.LstdFlags | log.Lshortfile)
+//}
+//
+//func init() {
+//	flags()
+//}
+//
+//func main() {
+//	flag.Parse()
+//
+//	var (
+//		response = new(models.ResponsePayload)
+//		result   []byte
+//		err      error
+//	)
+//	switch method {
+//	case "GetState":
+//		if response, err = GetState(bulbIp); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "GetConfig":
+//		if response, err = GetConfig(bulbIp); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "TurnOnLight":
+//		if response, err = TurnOnLight(bulbIp); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "TurnOffLight":
+//		if response, err = TurnOffLight(bulbIp); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "SetColorTemp":
+//		if response, err = SetColorTemp(bulbIp, value); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "SetBrightness":
+//		if response, err = SetBrightness(bulbIp, value); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "SetColorRGB":
+//		if response, err = SetColorRGB(bulbIp, r, g, b); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "SetColorScene":
+//		if response, err = SetColorScene(bulbIp, sceneId); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "SetColorWarmWhite":
+//		if response, err = SetColorWarmWhite(bulbIp, value); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	case "SetColorColdWhite":
+//		if response, err = SetColorColdWhite(bulbIp, value); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	default:
+//		if response, err = GetState(bulbIp); err != nil {
+//			log.Fatalf(`Unable to read response: %s`, err)
+//		}
+//	}
+//	if result, err = json.Marshal(response); err != nil {
+//		log.Fatalf(`Unable to convert to json string: %s`, err)
+//	}
+//	fmt.Println(string(result))
+//	os.Exit(0)
+//}
 
 func GetState(bulbIp string) (*models.ResponsePayload, error) {
 	var (
